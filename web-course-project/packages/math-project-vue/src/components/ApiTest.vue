1b55cf518b193efa86292ad8e44cd8ef10023e2a
Why-Five <why1605044412@163.com>
2025-09-13 19:18:01 +0800

    Eslint扁平化，hooks没搞

--- web-course-project/packages/math-project-vue/src/components/ApiTest.vue
+++ web-course-project/packages/math-project-vue/src/components/ApiTest.vue
@@ -14,7 +14,7 @@ const { loading, error, fetchData } = useApi()
 const songs = ref<Song[]>([])
 
 const getData = async () => {
-    const result = await fetchData('/api/songs')
+  const result = await fetchData('/api/songs')
   if (result) {
     songs.value = result
   }
@@ -30,17 +30,17 @@ onMounted(async () => {
     <div v-if="loading">加载中...</div>
     <div v-else-if="error">错误: {{ error }}</div>
     <div v-else>
-        <div v-for="item in songs" :key="item.id">
-            <div class="song-item">
-                <img :src="item.img" alt="封面">
-                <div class="song-info">
-                    <div class="song-name">{{ item.songName }}</div>
-                    <div class="song-singer">{{ item.singer }}</div>
-                    <div class="song-date">{{ item.date }}</div>
-                </div>
-                </img>
-            </div>
+      <div v-for="item in songs" :key="item.id">
+        <div class="song-item">
+          <img :src="item.img" alt="封面">
+          <div class="song-info">
+            <div class="song-name">{{ item.songName }}</div>
+            <div class="song-singer">{{ item.singer }}</div>
+            <div class="song-date">{{ item.date }}</div>
+          </div>
+          </img>
         </div>
+      </div>
     </div>
   </div>
 </template>
